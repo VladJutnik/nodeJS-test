2362979b9fc4297806ef09a57ef6c32d092083b6
VladJutnik <v2517v@gmail.com>
2022-03-11 16:51:21 +0700

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,24 @@
 const { Router } = require('express')
 const router = Router()
 
+//req.ip - IP адрес пользователя
+//req.query - содержит объект  параметров пример {get1: '1', get2: '2'}
+//req.path - адрес без протокола, хоста, порта и строки запроса
+//req.url - получить запрошенный адрес с путем и строкой запроса
+//res.redirect('/'); перенаправление на другую страницу  По умолчанию код редиректа будет 302 (найдено) res.redirect(301, '/');
+//res.type('text/plain'); type можно сменить тип ответа
 router.get('/', function(req, res) {
-    res.send('hello world 2 работают пути ');
+    res.type('text/plain');
+    res.send('hello world 2 работают пути ');//возращаем ответ
+    //res.status(403).send('hello world');
     console.log(req.path)
+    console.log(req.url)
+    console.log(req.query)
+    console.log(res.type)
 });
-
+router.get('/page/sfdsdf/', function(req, res) {
+    res.redirect(301, '/');
+});
 router.get('/1/', function(req, res) {
     res.send('page1');
 });
@@ -21,5 +34,7 @@ router.get('/3/', function(request, responce ) {
 router.get('/4/', function(request, responce) {
     responce.send('page5');
 });
-
+router.use(function(req, res) {
+    res.status(404).send('not found');
+});
 module.exports = router
