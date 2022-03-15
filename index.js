4991c102266f9ae044936fff52347ae1d094dd32
VladJutnik <v2517v@gmail.com>
2022-03-15 14:00:39 +0700

    ready routes

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const Routes = require('./routes/routes')
 
 let app = express();
 app.use(Routes)
-
+app.get('/p/:tagId', function(req, res) {
+    res.send("tagId is set to " + req.params.tagId);
+});
+app.get('/about', function (req, res) {
+    res.send('About this wiki');
+})
+app.use(function (req, res) {
+    res.status(404).send('not found');
+});
 async function start() {
     try {
         app.listen(3000, function() {
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express')
+const {Router} = require('express')
 const router = Router()
 
 //req.ip - IP адрес пользователя
@@ -7,7 +7,7 @@ const router = Router()
 //req.url - получить запрошенный адрес с путем и строкой запроса
 //res.redirect('/'); перенаправление на другую страницу  По умолчанию код редиректа будет 302 (найдено) res.redirect(301, '/');
 //res.type('text/plain'); type можно сменить тип ответа
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.type('text/plain');
     res.send('hello world 2 работают пути ');//возращаем ответ
     //res.status(403).send('hello world');
@@ -16,25 +16,55 @@ router.get('/', function(req, res) {
     console.log(req.query)
     console.log(res.type)
 });
-router.get('/page/sfdsdf/', function(req, res) {
+router.get('/page/sfdsdf/', function (req, res) {
     res.redirect(301, '/');
 });
-router.get('/1/', function(req, res) {
-    res.send('page1');
+router.get('/1/:ggg', function (req, res) {
+    let num = req.params.ggg;
+    if (num >= 1 && num <= 9)
+    {
+        res.send('your num: ' + num);
+    }
+    else
+    {
+        res.status(404).send('not found');
+    }
+    //res.send(num);
 });
 
-router.get('/2/', function(req, res) {
+router.get('/2/', function (req, res) {
     res.send('page2');
 });
 
-router.get('/3/', function(request, responce ) {
+router.get('/3/', function (request, responce) {
     responce.send('page4');
 });
 
-router.get('/4/', function(request, responce) {
+router.get('/4/', function (request, responce) {
     responce.send('page5');
 });
-router.use(function(req, res) {
-    res.status(404).send('not found');
+
+///:animal/about
+router.get('/:animal/about', function (req, res) {
+    // here we have bird or fish in req.params.animal
+    if (req.params.animal == 'bird')
+    {
+        res.send('Birds can fly');
+    }
+    else if (req.params.animal == 'fish')
+    {
+        res.send('Fish can swim');
+    }
+    else
+    {
+        res.send('Unknown animal');
+    }
 });
+
+router.get('/about/:param1/:param2', function (req, res) {
+    //then you can call this handler  through /about/1/sometext get these params from request object:
+    console.log(req.params.param1, req.params.param2); // 1, 'sometext'
+    res.send('About birds');
+});
+
 module.exports = router
